89c2ff09983f2bba1eb75f03bd566a45dd0f54cc
Kyle Kutner <kkutner01@mica.edu>
2017-12-11 08:16:06 -0500

    updates after critique

--- js/menu.js
+++ js/menu.js
@@ -4,16 +4,18 @@ var w = window.innerHeight;
 var h = window.innerHeight;
 
 
-document.getElementById('wrapper3').onclick = function() {
-
-    var className = ' ' + wrapper3.className + ' ';
-
-    this.className = ~className.indexOf(' active ') ?
-                         className.replace(' active ', ' ') :
-                         this.className + ' active';
-}
 
 
+// document.getElementById('wrapper3').onclick = function() {
+    
+
+// var className = ' ' + wrapper3.className + ' ';
+
+//     this.className = ~className.indexOf(' active ') ?
+//                          className.replace(' active ', ' ') :
+//                          this.className + ' active';
+
+// }
 
 
 var isOpen = true;
@@ -21,29 +23,53 @@ var isOpen = true;
 
 
 function openNav() {
-    document.getElementById("packagingnav").style.height = "100%";
-    document.getElementById("main").style.marginBottom = "0px";
-    document.getElementById("nutritionnav").style.height = "100%";
+    document.getElementById("totalnav").style.height = "100%";
+    document.getElementById("packagingnav").style.height = "900px";
+    document.getElementById("nutritionnav").style.height = "900px";
+    document.getElementById("blurb").style.height = "50%";
     isOpen = true; 
 
 }
 
 function closeNav() {
-    document.getElementById("packagingnav").style.height = "0";
-    document.getElementById("main").style.marginBottom= "30px";
-    document.getElementById("nutritionnav").style.height = "0";
+    document.getElementById("totalnav").style.height = "0%";
+    document.getElementById("packagingnav").style.height = "0px";
+    document.getElementById("nutritionnav").style.height = "0px";
+    document.getElementById("blurb").style.height = "0%";
     isOpen = false; 
 }
 
+
+// function openNav() {
+//     document.getElementById("totalnav").style.visibility = "visible";
+//     document.getElementById("packagingnav").style.visibility = "visible";
+//     document.getElementById("nutritionnav").style.visibility = "visible";
+//     document.getElementById("blurb").style.visibility = "visible";
+//     isOpen = true; 
+
+// }
+
+// function closeNav() {
+//     document.getElementById("totalnav").style.visibility = "hidden";
+//     document.getElementById("packagingnav").style.visibility = "hidden";
+//     document.getElementById("nutritionnav").style.visibility = "hidden";
+//     document.getElementById("blurb").style.visibility = "hidden";
+//     isOpen = false; 
+// }
+
+
 function toggleNav () {
-	if (isOpen==false){
+	if (isOpen===false){
 		openNav();
 	}
 	else{
 		closeNav();
+        document.getElementById('wrapper3').style.visibility = "visible";
 	}
 
 }
+
+console.log(isOpen);
 
 
 
